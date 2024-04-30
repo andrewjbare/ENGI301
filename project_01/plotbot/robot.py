d3100eded3619ecefdddadb17641236b31a7e635
Andrew Bare <ajbare224@gmail.com>
2024-04-30 17:37:44 -0500

    add documentation

--- project_01/plotbot/robot.py
+++ project_01/plotbot/robot.py
@@ -1,6 +1,43 @@
+"""
+--------------------------------------------------------------------------
+robot.py - singleton robot implementation (wip)
+--------------------------------------------------------------------------
+License:   
+Copyright 2024 - Andrew Bare
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+"""
+
 import Adafruit_BBIO.GPIO as GPIO
 
 import time
+import math
+
+STEP_LENGTH = 1 # Empirical value that must be set once we get the actual length of a step on the robot
 
 class Stepper:
     def __init__(self, pins, states):
@@ -33,7 +70,7 @@ class Stepper:
         self.write_pins(self.states[self.lead_pin])
         # Increment lead pin so the next run steps to the next state. If we've
         # hit the end of states, go back to state 0.
-        if self.lead_pin < len(self.states):
+        if self.lead_pin < len(self.states) - 1:
             self.lead_pin += 1
         else:
             self.lead_pin = 0
@@ -54,32 +91,13 @@ class Robot:
         pass
 
     def move(self, distance):
-        #for i in range(1,100):
-        self.left_stepper.step()
-        self.right_stepper.step()
-
-    def arcmove(self, distance, angle) -> None:
-        """Move distance arclength and end at an angle differing by angle"""
-        pass
-
-    def dwell(self, time_s) -> None:
-        """Wait time_s seconds"""
-        pass
-
-    def bell(self, time_s) -> None:
-        """Sound the buzzer"""
-        pass
+        step_count = math.floor(distance / STEP_LENGTH)
+        for i in range(1, step_count):
+            self.left_stepper.step()
+            self.right_stepper.step()
 
     def zmove(self, distance) -> None:
         """Move the pen (Z)"""
-        pass
-
-    def lightOn(self) -> None:
-        """Turn light on"""
-        pass
-
-    def lightOff(self) -> None:
-        """Turn light off"""
         pass
 
 
